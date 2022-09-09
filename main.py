3f8b99a8ab55cca517998d87506b4fd3e12aa758
aliaqeelalmuharrqi <N09335@bapco.net>
2022-09-09 17:22:28 +0300

    Implement GUI

--- main.py
+++ main.py
@@ -1,31 +1,74 @@
 import os
-from tkinter.filedialog import askdirectory
+from tkinter import *
+from tkinter import filedialog
 
-path = askdirectory()
-entries = os.listdir(path)
+gui_win = Tk()
+gui_win.title('Episode-Subtitle Matcher')
+gui_win.geometry('400x300')
+gui_win.grid_rowconfigure(0, weight=1)
+gui_win.grid_columnconfigure(0, weight=1)
 
-videoTypes = ['.mkv', '.mp4']
-subtitleIndex = 0
-numberOfEpisodes = 0
-episodeType = ''
-episodesNames = []
+label = StringVar()
+errorLabel = StringVar()
 
-for entry in entries:
-    if any(x in entry for x in videoTypes):
-        numberOfEpisodes += 1
-        episodesNames.append(entry)
 
-if episodesNames[0].endswith('.mkv'):
-    episodeType = '.mkv'
-elif episodesNames[0].endswith('.mp4'):
-    episodeType = '.mp4'
+def directory():
+    path = filedialog.askdirectory(title="Select The Folder")
+    if path == '':
+        return
+    label.set(path)
+    entries = os.listdir(path)
 
-for entry in entries:
-    if entry.endswith('.srt'):
-        subtitleFilePath = path + '\\' + entry
-        episodesNamesPath = path + '\\' + episodesNames[subtitleIndex]
-        episodesNamesPath = episodesNamesPath.replace(episodeType, '.srt')
-        os.rename(subtitleFilePath, episodesNamesPath)
-        subtitleIndex += 1
-        if subtitleIndex > numberOfEpisodes:
-            break
+    videoTypes = ['.mkv', '.mp4']
+    episodeType = ''
+    episodesNames = []
+    numberOfEpisodes = 0
+
+    subtitleIndex = 0
+    subtitleTypes = ['.srt', '.ass']
+    subtitleType = ''
+
+    try:
+        for entry in entries:
+            if any(x in entry for x in videoTypes):
+                numberOfEpisodes += 1
+                episodesNames.append(entry)
+
+        if episodesNames[0].endswith('.mkv'):
+            episodeType = '.mkv'
+        elif episodesNames[0].endswith('.mp4'):
+            episodeType = '.mp4'
+
+        for entry in entries:
+            if any(x in entry for x in subtitleTypes):
+                if entry.endswith('.srt'):
+                    subtitleType = '.srt'
+                elif entry.endswith('.ass'):
+                    subtitleType = '.ass'
+
+        for entry in entries:
+            if entry.endswith(subtitleType):
+                subtitleFilePath = path + '\\' + entry
+                episodesNamesPath = path + '\\' + episodesNames[subtitleIndex]
+                episodesNamesPath = episodesNamesPath.replace(episodeType, subtitleType)
+                os.rename(subtitleFilePath, episodesNamesPath)
+                subtitleIndex += 1
+                if subtitleIndex > numberOfEpisodes:
+                    break
+        errorLabel.set('Successfully Renamed All Subtitles')
+        error_label.config(fg='green')
+    except:
+        errorLabel.set('Sorry, Something went wrong')
+        error_label.config(fg='red')
+
+
+dialog_btn = Button(gui_win, text='select a folder', command=directory, width=15, height=2)
+dialog_btn.pack(pady=20)
+
+label_path = Label(gui_win, textvariable=label, font='italic 8', wraplength=350, justify=CENTER)
+label_path.pack(pady=20)
+
+error_label = Label(gui_win, textvariable=errorLabel, font='italic 8')
+error_label.pack(pady=20)
+
+gui_win.mainloop()
